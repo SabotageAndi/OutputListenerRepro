b4a351c58e9547f2ea023778fe45d470243368a1
Andreas Willich <andreas@willich.io>
2020-12-18 10:22:36 +0100

    Add project files.

---
+++ OutputListenerRepro/Hooks/Hooks.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BoDi;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Infrastructure;
+
+namespace OutputListenerRepro.Hooks
+{
+    [Binding]
+    public class Hooks
+    {
+        [BeforeScenario(Order = 1)]
+        public void RegisterDependencies(IObjectContainer objectContainer)
+        {
+            objectContainer.RegisterTypeAs<CustomOutputListener, ISpecFlowScenarioOutputListener>("customlistener");
+        }
+    }
+
+    public class CustomOutputListener : ISpecFlowScenarioOutputListener
+    {
+        public void OnMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ OutputListenerRepro/Steps/CalculatorStepDefinitions.cs
@@ -0,0 +1,45 @@
+﻿using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Infrastructure;
+
+namespace OutputListenerRepro.Steps
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        private readonly ScenarioContext _scenarioContext;
+        private readonly ISpecFlowOutputHelper _specFlowOutputHelper;
+
+        public CalculatorStepDefinitions(ScenarioContext scenarioContext, ISpecFlowOutputHelper specFlowOutputHelper)
+        {
+            _scenarioContext = scenarioContext;
+            _specFlowOutputHelper = specFlowOutputHelper;
+        }
+
+        [Given("the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+           _specFlowOutputHelper.WriteLine("test");
+        }
+
+        [Given("the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int number)
+        {
+           
+        }
+
+        [When("the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+           
+        }
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+           
+        }
+    }
+}
